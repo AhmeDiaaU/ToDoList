85dd15208c263c11f80416ea9349408e81f5db80
AhmeDiaaU <ahmeddeya58@gmail.com>
2025-05-08 22:03:07 +0300

    sql update

--- app/Schemma.sql
+++ app/Schemma.sql
@@ -1,10 +1,9 @@
-Create DATABASE todo;
+CREATE DATABASE IF NOT EXISTS todo;
 USE todo;
-CREATE TABLE accounts (
+
+CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    username VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE tasks (
@@ -12,10 +11,9 @@ CREATE TABLE tasks (
     title VARCHAR(100) NOT NULL,
     complete BOOLEAN DEFAULT FALSE,
     archived BOOLEAN DEFAULT FALSE,
-    Days_of_week VARCHAR(20) NOT NULL,
     Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES accounts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES  user(id) ON DELETE CASCADE,
     UNIQUE (user_id, title)
 );
 
@@ -31,4 +29,33 @@ CREATE TABLE task_category (
     FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
     FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
     UNIQUE (category_id, task_id) 
+);
+CREATE TABLE user_profile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    profile_picture VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+CREATE TABLE tasks_Schedule(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_id INT NOT NULL,
+    day_of_week ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    repeat_task BOOLEAN DEFAULT FALSE,
+    repeat_interval ENUM('Daily', 'Weekly', 'Monthly') DEFAULT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
 );
